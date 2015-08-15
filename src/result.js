16b132f6d60c9db7cdff09ba71e6041899119dc8
brad <lyman.brad3211@gmail.com>
2015-08-15 19:41:09 -0400

    updated for cleaner control flow functions

--- src/result.js
+++ src/result.js
@@ -5,101 +5,51 @@
  * @module object-result
  **/
 
-/**
- * Result object which represents either a success (Ok) or failure
- * (Err) of an operation.
- * @typedef Result
- * @type {Object}
- * @property {(Object|undefined)} Ok
- * @property {(Object|undefined)} Err
- **/
+var createOkResult = function(ok) {
+  var my_ok = ok;
 
-/**
- * Callback for the Map operation on a Result.
- * @callback applyOkFn
- * @param {Object} Ok - Ok value of the Result.
- * @return {Object} Ok value for new Result.
- **/
+  return {
+    /** Returns err value or undefined */
+    get_err  : function() { return undefined; },
 
-/**
- * Creates a Result with Err set to the provided value.
- * @param {Object} err - Information about the error you wish to report.
- * @return {Result}
- **/
-module.exports.Err = function(err) {
-  return { Err : err };
+    /** Returns ok value or undefined  */
+    get_ok   : function() { return my_ok; },
+
+    /** True if this represents an error */
+    is_err   : function() { return false; },
+
+    /** Execute callback if this is an error */
+    or_else  : function() { return this; },
+
+    /** Execute callback if this is an ok */
+    and_then : function(cb) {
+      return cb(my_ok) || this;
+    }
+  };
 };
 
-/**
- * Creates a Result with Ok set to the provided value.
- * @param {Object} ok - Information about the successful operation.
- * @return {Result}
- **/
-module.exports.Ok = function(ok) {
-  return { Ok : ok };
+var createErrResult = function(err) {
+  var my_err = err;
+
+  return {
+    /** Returns err value or undefined */
+    get_err  : function() { return my_err; },
+
+    /** Returns ok value or undefined */
+    get_ok   : function() { return undefined; },
+
+    /** True if this represents an error */
+    is_err   : function() { return true; },
+
+    /** Execute callback if this is an ok */
+    and_then : function() { return this; },
+
+    /** Execute callback if this is an error */
+    or_else  : function(cb) {
+      return cb(my_err) || this;
+    }
+  };
 };
 
-var is_err =
-/**
- * Returns true if the Result is an error.
- * @param {Result} result
- * @return {Boolean}
- **/
-module.exports.is_err = function(result) {
-  return result.Err !== undefined;
-};
-
-var is_ok =
-/**
- * Returns true if the Result is ok.
- * @param {Result} result
- * @return {Boolean}
- **/
-module.exports.is_ok = function(result) {
-  return result.Ok !== undefined;
-};
-
-/**
- * Modifies the Ok value of the Result by applying the function to the
- * Ok value, leaving an Err value untouched.
- * @param {Result} result - Result to map.
- * @param {applyOkFn} cb - Function to apply to the Ok value.
- * @return {Result}
- **/
-module.exports.map = function(result, cb) {
-  if (is_ok(result)) {
-    result.Ok = cb(result.Ok);
-  }
-
-  return result;
-};
-
-/**
- * Modifies the Err value of the Result by applying the function to the
- * Err value, leaving an Ok value untouched.
- * @param {Result} result - Result to map.
- * @param {applyErrFn} cb - Function to apply to the Err value.
- * @return {Result}
- **/
-module.exports.map_err = function(result, cb) {
-  if (is_err(result)) {
-    result.Err = cb(result.Err);
-  }
-
-  return result;
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports.Ok  = createOkResult;
+module.exports.Err = createErrResult;
